e5864d96fd1e10fbfc9c2fc27c220bda887f5d64
James Gould <james.gould@kincentric.com>
2020-04-02 12:01:01 -0500

    Added api tests with query parameters

--- cypress/integration/smoke_test_spec.js
+++ cypress/integration/smoke_test_spec.js
@@ -7,16 +7,68 @@ const postPayload = {
   score: 5
 };
 
-describe.only("API connection test", () => {
-  it("GET request to backend", () => {
+describe("API connection test", () => {
+  it("GET request test", () => {
     cy.request("http://localhost:5000/api/pizzas").then(response => {
       expect(response.status).to.eq(200);
       expect(response.body.length).to.greaterThan(0);
     });
   });
-  it("POST request to backend", () => {
+  it("GET request with RESTAURANT query", () => {
+    const input = "Young Joni";
+    cy.request(`http://localhost:5000/api/pizzas?restaurant=${input}`).then(
+      response => {
+        expect(response.status).to.eq(200);
+        response.body.forEach(element => {
+          expect(element.restaurant).to.eq(input);
+        });
+      }
+    );
+  });
+  it("GET request with SCORE query", () => {
+    const score = "5";
+    cy.request(`http://localhost:5000/api/pizzas?score=${score}`).then(
+      response => {
+        expect(response.status).to.eq(200);
+        response.body.forEach(element => {
+          expect(element.restaurant).to.eq(input);
+        });
+      }
+    );
+  });
+  it("GET request with PIZZA query", () => {
+    const pizza = "Pepperoni";
+    cy.request(`http://localhost:5000/api/pizzas?pizza=${pizza}`).then(
+      response => {
+        expect(response.status).to.eq(200);
+        response.body.forEach(element => {
+          expect(element.restaurant).to.eq(input);
+        });
+      }
+    );
+  });
+  it("GET request with STYLE query", () => {
+    const style = "Classic";
+    cy.request(`http://localhost:5000/api/pizzas?style=${style}`).then(
+      response => {
+        expect(response.status).to.eq(200);
+        response.body.forEach(element => {
+          expect(element.restaurant).to.eq(input);
+        });
+      }
+    );
+  });
+  it("GET request with no results", () => {
+    const xyz = "xyz";
+    cy.request(`http://localhost:5000/api/pizzas?xyz=${xyz}`).then(response => {
+      expect(response.status).to.eq(200);
+      expect(response.body.length).to.eq(0);
+    });
+  });
+  it("POST request test", () => {
     cy.request("POST", "http://localhost:5000/api/pizzas", postPayload).then(
       response => {
+        console.log(response);
         expect(response.status).to.eq(201);
       }
     );
